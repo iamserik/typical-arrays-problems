8466dc1b94b9f16cdc80bfabd10da2945dd22198
Serik <serj_navii@mail.ru>
2021-02-05 10:23:28 +0600

    init: write functions min, max, avg in index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0) return 0;
+  let m = array[0];
+  for(let i=1; i<array.length; i++)
+      m = array[i] > m ? m : array[i];
+  return m;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0) return 0;
+  let m = array[0];
+  for(let i=1; i<array.length; i++)
+      m = array[i] < m ? m : array[i];
+  return m;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0) return 0;
+  let a = 0;
+  for(let i=0; i<array.length; i++)
+      a += array[i];
+  return (a/array.length);
 }
